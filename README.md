a5d65e76c0f4a3950262e94f30687c6a5777f35d
rachele <rachelecerasoli6@gmail.com>
2023-11-14 22:37:58 +0100

    second commit

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Exercise 1 
+# Exercise 2
 
-* Create four variables (`brand`, `model`, `production year`, `owner's name`) and assign them a value. Use let and const where appropriate.
-* Create a variable to define if a car is new or not new.
-* Use the `number`, `boolean` and `string` data type to create the variables.
-* If you use a name with more than one word, remember to use the camel case naming convention.
+Write a better names for variables where it's necessary.
+
+
+1. let _name = 'mario';
+2. let *surname = 'Rossi';
+3. let 1age = 20;
+4. let ?isMarried = false;
+5. let job? = 'developer';
+6. let isGraduated2 = false;
---
+++ esercizio2.js
@@ -0,0 +1,6 @@
+let firstName = "Mario";      // _name in firstName per rappresentare il primo nome della persona.
+let lastName = "Rossi";       //*surname in lastName per rappresentare il cognome della persona.
+let age1 = 20;                //I nomi delle variabili non possono iniziare con un numero in JavaScript.
+let isMarried = false;        //I simboli di domanda o punto interrogativo non sono ammessi nei nomi delle variabili.
+let jobTitle = "developer";   //I simboli di domanda o punto interrogativo non sono ammessi nei nomi delle variabili.
+let hasGraduated = false;     //isGraduated2 in hasGraduated per rappresentare lo stato di laurea della persona in modo più chiaro.
